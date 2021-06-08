ad2d1bbed12a2687d36c6e238dac24482de1e98a
Caden Wilcox <wilcox4357@gmail.com>
2021-06-07 18:56:27 -0700

    First assignment

---
+++ 01-java-fundamentals/05-basics/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My Name Is Caden Wilcox");
+        System.out.println("I am 23 years old");
+        System.out.println("My Hometown Is Seattle, Wa");
+    }
+}
